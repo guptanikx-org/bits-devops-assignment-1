93331dda825008178c2b5a5c4edf28f02c9c2257
nikx <113229040+guptanikx@users.noreply.github.com>
2024-02-24 16:46:36 +0530

    seperate test stage

---
+++ setup_runner.sh
@@ -0,0 +1,2 @@
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.313.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.313.0/actions-runner-linux-x64-2.313.0.tar.gz
